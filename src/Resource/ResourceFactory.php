b506ab7e44ce91444edf02f38b0547a2f243b1c7
Kayla Daniels <kayladnls@gmail.com>
2015-09-21 11:31:42 -0400

    Create Resource factory

---
+++ src/Resource/ResourceFactory.php
@@ -0,0 +1,43 @@
+<?php namespace Refinery29\ApiOutput\Resource;
+
+use Refinery29\ApiOutput\Resource\Error\Error;
+use Refinery29\ApiOutput\Resource\Error\ErrorCollection;
+use Refinery29\ApiOutput\Resource\Link\LinkCollection;
+
+abstract class ResourceFactory
+{
+    /**
+     * @param $data
+     * @return Result
+     */
+    public static function result(array $data)
+    {
+        return new Result($data);
+    }
+
+    /**
+     * @return ErrorCollection
+     */
+    public static function errorCollection()
+    {
+        return new ErrorCollection();
+    }
+
+    /**
+     * @param $title
+     * @param $code
+     * @return Error
+     */
+    public static function error($title, $code)
+    {
+        return new Error($title, $code);
+    }
+
+    /**
+     * @return LinkCollection
+     */
+    public static function linkCollection()
+    {
+        return new LinkCollection();
+    }
+}
