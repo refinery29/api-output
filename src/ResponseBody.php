abec481a2a08ab4341e3d32cb0df949fbe76b961
Andreas Möller <am@localheinz.com>
2017-05-09 06:22:35 +0200

    Fix: Run 'make cs'

--- src/ResponseBody.php
+++ src/ResponseBody.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * Copyright (c) 2017 Refinery29, Inc.
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
 namespace Refinery29\ApiOutput;
 
 use Refinery29\ApiOutput\Resource\TopLevelResource;
@@ -32,17 +39,17 @@ class ResponseBody
     public function getOutput()
     {
         $response = [];
-        reset($this->members);
-        $member = current($this->getMembers());
+        \reset($this->members);
+        $member = \current($this->getMembers());
 
         if ($member instanceof CustomResult) {
-            return json_encode((object) $member->getOutput(), JSON_UNESCAPED_SLASHES);
+            return \json_encode((object) $member->getOutput(), JSON_UNESCAPED_SLASHES);
         }
 
         foreach ($this->members as $member) {
             $response[$member->getTopLevelName()] = $member->getOutput();
         }
 
-        return json_encode((object) $response, JSON_UNESCAPED_SLASHES);
+        return \json_encode((object) $response, JSON_UNESCAPED_SLASHES);
     }
 }
--- src/Serializer/Link/LinkCollection.php
+++ src/Serializer/Link/LinkCollection.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * Copyright (c) 2017 Refinery29, Inc.
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
 namespace Refinery29\ApiOutput\Serializer\Link;
 
 use Exception;
@@ -55,7 +62,8 @@ class LinkCollection implements TopLevelResource
     }
 
     /**
-     * @param Link $link
+     * @param Link  $link
+     * @param mixed $output
      *
      * @return \stdClass
      */
@@ -69,7 +77,8 @@ class LinkCollection implements TopLevelResource
     }
 
     /**
-     * @param Link $link
+     * @param Link  $link
+     * @param mixed $output
      *
      * @return \stdClass
      */
